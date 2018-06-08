a123bfc58d1361d1b748d45fc9909b51654b0666
刘畅 <s1329237555@live.com>
2018-06-08 20:45:03 +0800

    clean

--- app/src/main/java/com/tongji/tcloud/iview/ICarousePagerSelectView.java
+++
@@ -1,5 +0,0 @@
-package com.tongji.tcloud.iview;
-
-public interface ICarousePagerSelectView {
-	public void carouseSelect(int index);//点击轮播图片
-}
--- app/src/main/java/com/tongji/tcloud/model/Directory.java
+++
@@ -1,9 +0,0 @@
-package com.tongji.tcloud.model;
-
-public class Directory{
-    public String type;
-    public String name;
-    public String permission;
-    public String path;
-    public String time;
-}
